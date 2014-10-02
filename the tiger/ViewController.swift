53b77840d8cac24bd5e8409090e3e8f781379e67
Gilbert <mont-phil@hotmail.fr>
2014-10-02 00:06:44 -0400

    Initial Commit

---
+++ the tiger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  the tiger
+//
+//  Created by GILBERT  on 2014-10-02.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
